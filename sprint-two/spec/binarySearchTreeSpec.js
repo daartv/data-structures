4b1ac57c5351dcb28d17a566a67b39fffbd9ef7d
Dario Arteaga <daartv@gmail.com>
2017-02-12 14:58:52 -0800

    Passes added test for binary search tree

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -28,6 +28,29 @@ describe('binarySearchTree', function() {
     expect(binarySearchTree.contains(8)).to.equal(false);
   });
 
+  it('should contain all of the children that were added, no matter how many', function() {
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(8);
+    binarySearchTree.insert(5);
+    binarySearchTree.insert(9);
+    binarySearchTree.insert(10);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(4);
+    binarySearchTree.insert(6);
+    expect(binarySearchTree.contains(2)).to.equal(true);
+    expect(binarySearchTree.contains(3)).to.equal(true);
+    expect(binarySearchTree.contains(7)).to.equal(true);
+    expect(binarySearchTree.contains(8)).to.equal(true);
+    expect(binarySearchTree.contains(5)).to.equal(true);
+    expect(binarySearchTree.contains(9)).to.equal(true);
+    expect(binarySearchTree.contains(10)).to.equal(true);
+    expect(binarySearchTree.contains(1)).to.equal(true);
+    expect(binarySearchTree.contains(4)).to.equal(true);
+    expect(binarySearchTree.contains(6)).to.equal(true);
+  });
+
   it('should execute a callback on every value in a tree using "depthFirstLog"', function() {
     var array = [];
     var func = function(value) { array.push(value); };
